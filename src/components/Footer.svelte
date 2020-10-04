5712bc77564416ca66c21edffcf6909328bab948
Maxim Siebert <maxim.siebert@gmail.com>
2020-10-04 15:13:42 -0400

    removed carousel

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
-<footer class="px-4 py-3 lg:mb-8 border-t border-gray-200 flex text-gray-500 uppercase text-xs tracking-wider font-medium">
+<footer class="px-4 py-3 border-t border-gray-200 flex text-gray-500 uppercase text-xs tracking-wider font-medium">
 	<div class="ml-auto lg:w-10/12 w-full flex">
 		<div class="lg:w-4/12 w-1/2">
-			<a class="hover:text-black" href="mailto:maxim@hey.com">maxim@hey.com</a>
+			<a class="transition-colors duration-100 hover:text-black" href="mailto:maxim@hey.com">maxim@hey.com</a>
 		</div>
 		<div class="lg:block hidden w-4/12">
 			<p>Updated on 10/20</p>
